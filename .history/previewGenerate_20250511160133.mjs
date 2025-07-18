5c22f2b9a40deda49a35754006c3bf7e2a2345b8
RMou <ronyjmolina@gmail.com>
2025-07-18 10:59:00 -0600

    updateV no history

--- .history/previewGenerate_20250511160133.mjs
+++
@@ -1,8 +0,0 @@
-
-
-
-
-path.resolve(__dirname, 'videos', 'https://cmsstorage.s3.us-east-2.amazonaws.com/videoserei/Wall+Facing+Unilateral+Open+Chain+Hip+ROM.mp4'),
-path.resolve(__dirname, 'videos', 'https://cmsstorage.s3.us-east-2.amazonaws.com/videoserei/Wall+Sit+%2B+Pandiculation+Hold.mp4'),
-path.resolve(__dirname, 'videos', 'https://cmsstorage.s3.us-east-2.amazonaws.com/videoserei/Wall+Sit+Variation.mp4'),
-path.resolve(__dirname, 'videos', 'https://cmsstorage.s3.us-east-2.amazonaws.com/videoserei/Wrist+Gyroscope+and+Finger+Glides.mp4')
